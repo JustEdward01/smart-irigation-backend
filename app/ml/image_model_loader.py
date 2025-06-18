7fa98666a44b64073f64d1f6508c4527d9063074
Edward <161495650+JustEdward01@users.noreply.github.com>
2025-06-18 16:54:12 +0300

    Add files via upload

--- app/ml/image_model_loader.py
+++ app/ml/image_model_loader.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
 from app.ml.symptom_action_map import SYMPTOM_ACTION_MAP
+from keras.layers import BatchNormalization
+
+def _bn_from_config(config):
+    # Dacă axis e listă, reține doar primul element
+    axis = config.get("axis")
+    if isinstance(axis, (list, tuple)):
+        config["axis"] = axis[0]
+    return BatchNormalization.from_config(config)
 
 plant_index_to_class = {
     0: "healthy",
@@ -15,4 +23,6 @@ MODEL_PATH = tf.keras.utils.get_file(
     "https://huggingface.co/Eduard009/smartplant-model/resolve/main/symptom_cnn_model.h5"
 )
 
-plant_model = tf.keras.models.load_model(MODEL_PATH)
+- plant_model = tf.keras.models.load_model(MODEL_PATH)
++ plant_model = tf.keras.models.load_model(
++     MODEL_PATH,custom_objects={"BatchNormalization": _bn_from_config})
