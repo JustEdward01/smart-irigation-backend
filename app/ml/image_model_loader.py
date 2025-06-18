6a61a3c067668a097af2b4ae81e94e3497c67946
Edward <161495650+JustEdward01@users.noreply.github.com>
2025-06-18 14:13:13 +0300

    Add files via upload

--- app/ml/image_model_loader.py
+++ app/ml/image_model_loader.py
@@ -1,21 +1,22 @@
 # app/ml/image_model_loader.py
 import tensorflow as tf
 import numpy as np
+from app.ml.symptom_action_map import SYMPTOM_ACTION_MAP
 
-# Calea către modelul salvat pe HuggingFace sau local
-MODEL_PATH = "https://huggingface.co/Eduard009/smartplant-model/resolve/main/symptom_cnn_model.h5"
+# Descărcare și salvare locală (doar dacă nu există deja)
+MODEL_PATH = tf.keras.utils.get_file(
+    "symptom_cnn_model.h5",
+    "https://huggingface.co/Eduard009/smartplant-model/resolve/main/symptom_cnn_model.h5"
+)
 
-# Poți folosi keras.utils.get_file dacă e online
-plant_model = tf.keras.models.load_model(tf.keras.utils.get_file("symptom_model", MODEL_PATH))
+# Încărcare model h5 (compatibil Keras 3)
+plant_model = tf.keras.models.load_model(MODEL_PATH)
 
+# Alte constante
 plant_index_to_class = {
     0: "healthy",
     1: "yellow_leaves",
     2: "spots_mold",
     3: "wilting"
 }
-
 IMG_SIZE = (224, 224)
-
-# Nu mai redefinim SYMPTOM_ACTION_MAP, îl importăm
-from app.ml.symptom_action_map import SYMPTOM_ACTION_MAP
